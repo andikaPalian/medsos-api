fa12add9f5112c7bbce625a40de9030c406eb4c5
Andika Palian <andika12des@gmail.com>
2025-10-19 16:18:15 +0800

    feat: add new validate schema for user auth

--- src/api/v1/auth/userAuth.routes.js
+++ src/api/v1/auth/userAuth.routes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { forgotPasswordController, loginController, registerController, resendVerificationEmailController, resetPasswordController, verifyEmailController } from "./userAuth.controller.js";
 import { validateBody } from "../../../middlewares/zodValidator.js";
-import { loginSchema, registerSchema } from "../../../validators/userAuthValidation.js";
+import { forgotPasswordSchema, loginSchema, registerSchema, resendVerificationEmailSchema, resetPasswordSchema, verifyEmailSchema } from "../../../validators/userAuthValidation.js";
 
 export const userAuthRouter = express.Router();
 
 userAuthRouter.post("/register", validateBody(registerSchema), registerController);
-userAuthRouter.post("/verify-email", verifyEmailController);
-userAuthRouter.post("/resend-verification", resendVerificationEmailController);
+userAuthRouter.post("/verify-email", validateBody(verifyEmailSchema), verifyEmailController);
+userAuthRouter.post("/resend-verification", validateBody(resendVerificationEmailSchema), resendVerificationEmailController);
 userAuthRouter.post("/login", validateBody(loginSchema), loginController);
-userAuthRouter.post("/forgot-password", forgotPasswordController);
-userAuthRouter.post("/reset-password", resetPasswordController);
+userAuthRouter.post("/forgot-password", validateBody(forgotPasswordSchema), forgotPasswordController);
+userAuthRouter.post("/reset-password", validateBody(resetPasswordSchema), resetPasswordController);
--- src/validators/userAuthValidation.js
+++ src/validators/userAuthValidation.js
@@ -3,12 +3,30 @@ import {z} from "zod";
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
 export const registerSchema = z.object({
-    username: z.string().min(3, "Username must be at least 3 characters long").max(50, "Username must be at most 50 characters long").trim(),
-    email: z.string().email("Invalid email address").trim().toLowerCase(),
+    username: z.string().min(3, "Username must be at least 3 characters long").max(30, "Username must be at most 30 characters long").trim(),
+    email: z.email("Invalid email address").trim().toLowerCase(),
     password: z.string().regex(passwordRegex, "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character").trim()
 });
 
 export const loginSchema = z.object({
-    email: z.string().email("Invalid email address").trim().toLowerCase(),
+    email: z.email("Invalid email address").trim().toLowerCase(),
     password: z.string().regex(passwordRegex, "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character").trim()
+});
+
+export const verifyEmailSchema = z.object({
+    email: z.email().trim(),
+    otp: z.string().trim()
+});
+
+export const resendVerificationEmailSchema = z.object({
+    email: z.email().trim()
+});
+
+export const forgotPasswordSchema = z.object({
+    email: z.email().trim()
+});
+
+export const resetPasswordSchema = z.object({
+    token: z.string().trim(),
+    newPassword: z.string().regex(passwordRegex, "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character").trim()
 });
