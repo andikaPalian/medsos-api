3bb8794842d1c74576fd5eed8256cd94db719d01
Andika Palian <andika12des@gmail.com>
2026-06-02 07:25:08 +0800

    feat: implement user authentication middleware with token verification and error handling

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,39 @@
+import { catchAsync } from "../utils/catchAsync.js";
+import { AppError } from "../utils/errorHandler.js";
+import { verifyToken } from "../utils/jwt.js";
+
+export const userAuth = catchAsync(async (req, res, next) => {
+  // First check fot access token in cookies
+  let token = req.cookies?.accessToken;
+
+  // If not found in cookies, check the Authorization header
+  if (!token && req.headers.authorization?.startsWith("Bearer ")) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  // If still not found, throw an error
+  if (!token) {
+    throw new AppError("Authentication failed. Access token is missing. Please login.", 401);
+  }
+
+  // Verify the token
+  try {
+    const decoded = verifyToken(token);
+
+    // Attach payload to request object
+    req.user = {
+      id: decoded.id,
+    };
+
+    // Continue to the next middleware
+    return next();
+  } catch (error) {
+    // Handle token verification errors
+    const errorMessage =
+      error.name === "TokenExpiredError"
+        ? "Unauthorized: Access token expired. Please refresh your session."
+        : "Unauthorized: Invalid token structure";
+
+    throw new AppError(errorMessage, 401);
+  }
+});
