35446be83e3261b8528ae76c66e4a18eb5e8d1e7
Andika Palian <andika12des@gmail.com>
2026-06-03 12:33:40 +0800

    feat: implement notification repository and service methods for CRUD operations

--- src/api/v1/notifications/notification.service.js
+++ src/api/v1/notifications/notification.service.js
@@ -1,210 +1,74 @@
-import prisma from "../../../config/client.js";
+import * as notificationRepository from "./notification.repository.js";
 import { AppError } from "../../../utils/errorHandler.js";
 
-export const createNotification = async (senderId, receiverId, type, postId, storyId) => {
-    try {
-        const sender = await prisma.user.findUnique({
-            where: {
-                id: senderId
-            }
-        });
-        if (!sender) {
-            throw new AppError("Sender not found", 404);
-        }
+// Service for create a notification
+export const createNotification = async ({ senderId, receiverId, type, postId, storyId }) => {
+  // Check the sender
+  const sender = await notificationRepository.findSenderById(senderId);
+  if (!sender) throw new AppError("Notification trigger failed: Sender not found", 404);
 
-        const receiver = await prisma.user.findUnique({
-            where: {
-                id: receiverId
-            }
-        });
-        if (!receiver) {
-            throw new AppError("Receiver not found", 404);
-        }
+  // Message template for each type
+  const messageTemplates = {
+    FOLLOW: `${sender.username} started following you.`,
+    FOLLOW_REQUEST: `${sender.username} sent you a follow request.`,
+    REQUEST_ACCEPTED: `${sender.username} accepted you follow request`,
+    LIKE: `${sender.username} liked your post`,
+    COMMENT: `${sender.username} commented on yout post`,
+    MENTION: `${sender.username} mentioned you in a post`,
+    MESSAGE: `${sender.username} sent you a message`,
+    STORY_VIEW: `${sender.username} viewed your story.`,
+    REQUEST_REJECTED: `${sender.username} rejected your follow request`,
+  };
 
-        let message = "";
-        switch (type) {
-            case "FOLLOW":
-                message = `${sender.username} started following you.`;
-                break;
-            
-            case "FOLLOW_REQUEST":
-                message = `${sender.username} sent you a follow request.`;
-                break;
+  const message = messageTemplates[type];
+  if (!message) throw new AppError("Invalid notification type", 400);
 
-            case "REQUEST_ACCEPTED":
-                message = `${sender.username} accepted your follow request.`;
-                break;
-
-            case "LIKE":
-                message = `${sender.username} liked your post.`;
-                break;
-
-            case "COMMENT":
-                message = `${sender.username} commented on your post.`;
-                break;
-
-            case "MENTION":
-                message = `${sender.username} mentioned you in a post.`;
-                break;
-
-            case "MESSAGE":
-                message = `${sender.username} sent you a message.`;
-                break;
-
-            case "STORY_VIEW":
-                message = `${sender.username} viewed your story.`;
-                break;
-
-            case "REQUEST_REJECTED":
-                message = `${sender.username} rejected your follow request.`;
-                break;
-        
-            default:
-                throw new AppError("Invalid notification type", 400);
-        }
-
-        // Make payload notification
-        const notificationData = {
-            userId: receiverId,
-            senderId: senderId,
-            type,
-            message,
-            isRead: false
-        };
-
-        if (postId) {
-            notificationData.postId = postId
-        }
-
-        if (storyId) {
-            notificationData.storyId = storyId;
-        }
-
-        const newNotification = await prisma.notification.create({
-            data: notificationData,
-            include: {
-                sender: {
-                    select: {
-                        id: true,
-                        profilePic: true,
-                        username: true
-                    }
-                }
-            }
-        });
-
-        return newNotification;
-    } catch (error) {
-        console.error("Error creating notification: ", error);
-        throw error;
-    }
+  return await notificationRepository.createNotification({
+    receiverId: receiverId,
+    senderId: senderId,
+    type: type,
+    message: message,
+    postId: postId,
+    storyId: storyId,
+  });
 };
 
-export const getNotifications = async (userId) => {
-    try {
-        const user = await prisma.user.findUnique({
-            where: {
-                id: userId
-            }
-        });
-        if (!user) {
-            throw new AppError("User not found", 404);
-        }
+// Service for get the notifications
+export const getNotifications = async (userId, { limit = 20, nextCursor = null } = {}) => {
+  const take = parseInt(limit, 10) || 20;
 
-        const notifications = await prisma.notification.findMany({
-            where: {
-                userId: userId
-            },
-            include: {
-                sender: {
-                    select: {
-                        id: true,
-                        profilePic: true,
-                        username: true
-                    }
-                }
-            },
-            orderBy: {
-                createdAt: "desc"
-            }
-        });
+  const notifications = await notificationRepository.findNotifications({
+    userId: userId,
+    take: take + 1,
+    nextCursor: nextCursor,
+  });
 
-        return notifications;
-    } catch (error) {
-        console.error("Error getting notifications: ", error);
-        throw error;
-    }
+  let hasNextPage = false;
+  let cursor = null;
+
+  if (notifications.length > take) {
+    hasNextPage = true;
+    const nextItem = notifications.pop();
+    cursor = nextItem.id;
+  }
+
+  return {
+    data: notifications,
+    pagination: {
+      hasNextPage,
+      nextCursor: cursor,
+    },
+  };
 };
 
+// Service for mark the notification as read
 export const markNotificationAsRead = async (userId, notificationId) => {
-    try {
-        const user = await prisma.user.findUnique({
-            where: {
-                id: userId
-            }
-        });
-        if (!user) {
-            throw new AppError("User not found", 404);
-        }
-
-        const notification = await prisma.notification.findUnique({
-            where: {
-                id: notificationId
-            }
-        });
-        if (!notification) {
-            throw new AppError("Notification not found", 404);
-        }
-
-        if (notification.userId !== userId) {
-            throw new AppError("Unauthorized: You can only mark your own notifications as read", 403);
-        }
-
-        await prisma.notification.update({
-            where: {
-                id: notificationId
-            },
-            data: {
-                isRead: true
-            }
-        });
-    } catch (error) {
-        console.error("Error marking notification as read: ", error);
-        throw error;
-    }
+  const updateResult = await notificationRepository.markNotificationAsRead(userId, notificationId);
+  if (updateResult.count === 0) throw new AppError("Notification not found or access denied", 404);
 };
 
+// Service for delete the notification
 export const deleteNotification = async (userId, notificationId) => {
-    try {
-        const user = await prisma.user.findUnique({
-            where: {
-                id: userId
-            }
-        });
-        if (!user) {
-            throw new AppError("User not found", 404);
-        }
-
-        const notification = await prisma.notification.findUnique({
-            where: {
-                id: notificationId
-            }
-        });
-        if (!notification) {
-            throw new AppError("Notification not found", 404);
-        }
-
-        if (notification.senderId !== userId) {
-            throw new AppError("Unauthorized: You can only delete your own notifications", 403);
-        }
-
-        await prisma.notification.delete({
-            where: {
-                id: notificationId
-            }
-        });
-    } catch (error) {
-        console.error("Error deleting notification: ", error);
-        throw error;
-    }
+  const result = await notificationRepository.deleteNotification(userId, notificationId);
+  if (result.count === 0) throw new AppError("Notification not found or empty", 404);
 };
