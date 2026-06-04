7de4bb9d3bbf9c2989d0ca24e80d6c85492c77dc
Andika Palian <andika12des@gmail.com>
2026-06-04 12:29:54 +0800

    feat: refactor notification routes to streamline controller imports and enhance user authentication

--- src/api/v1/notifications/notification.routes.js
+++ src/api/v1/notifications/notification.routes.js
@@ -1,10 +1,11 @@
-import express from "express";
-import { createNotificationController, deleteNotificationController, getNotificationsController, markNotificationAsReadController } from "./notification.controller.js";
-import { userAuth } from "../../../middlewares/userMiddleware.js";
+import { Router } from "express";
+import * as notificationController from "./notification.controller.js";
+import { userAuth } from "../../../middlewares/authMiddleware";
 
-export const notificationRouter = express.Router();
+export const notificationRouter = Router();
 
-notificationRouter.post("/", userAuth, createNotificationController);
-notificationRouter.get("/", userAuth, getNotificationsController);
-notificationRouter.patch("/:notificationId", userAuth, markNotificationAsReadController);
-notificationRouter.delete("/:notificationId", userAuth, deleteNotificationController);
+notificationRouter.use(userAuth);
+
+notificationRouter.get("/", notificationController.getUserNotifications);
+notificationRouter.patch("/:notificationId", notificationController.readNotification);
+notificationRouter.delete("/:notificationId", notificationController.deleteNotification);
