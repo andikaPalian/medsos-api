7174ff164bbbb32716e0811590d85244777438ba
Andika Palian <andika12des@gmail.com>
2026-07-04 18:11:56 +0800

    feat: add validation schemas to user authentication routes for improved data integrity

--- src/modules/auth/routes/userAuth.routes.ts
+++ src/modules/auth/routes/userAuth.routes.ts
@@ -2,28 +2,69 @@ import { Router } from "express";
 import * as userAuthController from "../controllers/userAuth.controller.js";
 import passport from "../../../config/passport.js";
 import { authLimiter, emailLimiter } from "../../../middlewares/rateLimiter.js";
+import { validate } from "../../../middlewares/validator.js";
+import {
+  completeOAuthRegistrationSchema,
+  emailVerificationSchema,
+  forgotPasswordSchema,
+  loginSchema,
+  logoutSchema,
+  refreshTokenSchema,
+  registerSchema,
+  resendVerificationSchema,
+  resetPasswordSchema,
+} from "../validators/userAuth.validation.js";
 
 export const userAuthRouter = Router();
 
 // Registration local route
-userAuthRouter.post("/register", authLimiter, userAuthController.registerController);
-userAuthRouter.post("/verify-email", userAuthController.verifyEmailController);
+userAuthRouter.post(
+  "/register",
+  authLimiter,
+  validate(registerSchema),
+  userAuthController.registerController,
+);
+userAuthRouter.post(
+  "/verify-email",
+  validate(emailVerificationSchema),
+  userAuthController.verifyEmailController,
+);
 userAuthRouter.post(
   "/resend-verification",
   emailLimiter,
+  validate(resendVerificationSchema),
   userAuthController.resendVerificationEmailController,
 );
 
 // Login and Logout
-userAuthRouter.post("/login", authLimiter, userAuthController.loginController);
-userAuthRouter.post("/logout", userAuthController.logoutController);
+userAuthRouter.post(
+  "/login",
+  authLimiter,
+  validate(loginSchema),
+  userAuthController.loginController,
+);
+userAuthRouter.post("/logout", validate(logoutSchema), userAuthController.logoutController);
 
 // Refresh Token
-userAuthRouter.post("/refresh-token", userAuthController.refreshTokenController);
+userAuthRouter.post(
+  "/refresh-token",
+  validate(refreshTokenSchema),
+  userAuthController.refreshTokenController,
+);
 
 // Password Management
-userAuthRouter.post("/forgot-password", emailLimiter, userAuthController.forgotPasswordController);
-userAuthRouter.post("/reset-password", authLimiter, userAuthController.resetPasswordController);
+userAuthRouter.post(
+  "/forgot-password",
+  emailLimiter,
+  validate(forgotPasswordSchema),
+  userAuthController.forgotPasswordController,
+);
+userAuthRouter.post(
+  "/reset-password",
+  authLimiter,
+  validate(resetPasswordSchema),
+  userAuthController.resetPasswordController,
+);
 
 // OAuth
 userAuthRouter.get(
@@ -41,5 +82,6 @@ userAuthRouter.get(
 userAuthRouter.post(
   "/complete-oauth",
   authLimiter,
+  validate(completeOAuthRegistrationSchema),
   userAuthController.completeOAuthRegistrationController,
 );
