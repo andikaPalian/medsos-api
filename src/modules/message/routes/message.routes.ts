2cfa5bacada632e155297c206a7fcde185ac0283
Andika Palian <andika12des@gmail.com>
2026-07-22 19:06:54 +0800

    refactor: update message validation imports for consistency

--- src/modules/message/routes/message.routes.ts
+++ src/modules/message/routes/message.routes.ts
@@ -1,34 +1,40 @@
 import { Router } from "express";
 import * as messageController from "../controllers/message.controller.js";
+import * as messageValidator from "../validators/message.validation.js";
 import { userAuth } from "../../../middlewares/auth.middleware.js";
 import { validate } from "../../../middlewares/validator.js";
-import {
-  attachmentIdParamSchema,
-  getMessageSchema,
-  messageIdParamSchema,
-  sendMessageSchema,
-  updateMessageSchema,
-} from "../validators/message.validation.js";
 
 export const messageRouter = Router();
 
 messageRouter.use(userAuth);
 
-messageRouter.post("/", validate(sendMessageSchema), messageController.sendMessage);
-messageRouter.get("/room/:roomId", validate(getMessageSchema), messageController.getMessages);
-messageRouter.patch("/:messageId", validate(updateMessageSchema), messageController.updateMessage);
+messageRouter.post(
+  "/",
+  validate(messageValidator.sendMessageSchema),
+  messageController.sendMessage,
+);
+messageRouter.get(
+  "/room/:roomId",
+  validate(messageValidator.getMessageSchema),
+  messageController.getMessages,
+);
+messageRouter.patch(
+  "/:messageId",
+  validate(messageValidator.updateMessageSchema),
+  messageController.updateMessage,
+);
 messageRouter.delete(
   "/:messageId/me",
-  validate(messageIdParamSchema),
+  validate(messageValidator.messageIdParamSchema),
   messageController.purgeMessageForMe,
 );
 messageRouter.delete(
   "/:messageId/everyone",
-  validate(messageIdParamSchema),
+  validate(messageValidator.messageIdParamSchema),
   messageController.recallMessageForEveryone,
 );
 messageRouter.get(
   "/:attachmentId/download",
-  validate(attachmentIdParamSchema),
+  validate(messageValidator.attachmentIdParamSchema),
   messageController.downloadAttachment,
 );
