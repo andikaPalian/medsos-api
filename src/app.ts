4a2280f156afd4ef39dc6b32dbe39b5e23662987
Andika Palian <andika12des@gmail.com>
2026-07-23 18:34:02 +0800

    feat: add report router to API for report functionality

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import { followRouter } from "./modules/follow/routes/follow.routes.js";
 import { postRouter } from "./modules/post/routes/post.routes.js";
 import { likeRouter } from "./modules/like/routes/like.routes.js";
 import { commentRouter } from "./modules/comment/routes/comment.routes.js";
+import { reportRouter } from "./modules/report/routes/report.routes.js";
 
 const API_PREFIX = "api/v1" as const;
 
@@ -70,6 +71,7 @@ export const createApp = (io: SocketServer) => {
   app.use(`${API_PREFIX}/post`, postRouter);
   app.use(`${API_PREFIX}/like`, likeRouter);
   app.use(`${API_PREFIX}/comment`, commentRouter);
+  app.use(`${API_PREFIX}/report`, reportRouter);
 
   app.use((_req: Request, res: Response) => {
     res.status(404).json({
