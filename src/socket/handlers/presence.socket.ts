bcabcd7ecb895fec1d3460bdd83ab6aa3f264c12
Andika Palian <andika12des@gmail.com>
2026-07-31 18:35:56 +0800

    feat: enhance presence handling by using co-participant notifications for online/offline events

--- src/socket/handlers/presence.socket.ts
+++ src/socket/handlers/presence.socket.ts
@@ -1,21 +1,28 @@
 import { markUserOffline, markUserOnline } from "../state/socket.presence.js";
 import { AppServer, AppSocket } from "../../common/types/socket.types.js";
 import { logger } from "../../common/utils/logger.js";
+import * as messageRepository from "../../modules/message/repositories/message.repository.js";
 
-export const registerPresenceHandlers = (io: AppServer, socket: AppSocket): void => {
+export const registerPresenceHandlers = async (io: AppServer, socket: AppSocket): Promise<void> => {
   const userId = socket.data.userId;
 
   socket.join(`user:${userId}`);
 
   const justCameOnline = markUserOnline(userId, socket.id);
   if (justCameOnline) {
-    socket.broadcast.emit("presence:online", { userId });
+    const coParticipants = await messageRepository.findCoParticipantIds(userId);
+    coParticipants.forEach((p) => {
+      io.to(`user:${p}`).emit("presence:online", { userId });
+    });
   }
 
-  socket.on("disconnect", () => {
+  socket.on("disconnect", async () => {
     const justWentOffline = markUserOffline(userId, socket.id);
     if (justWentOffline) {
-      socket.broadcast.emit("presence:offline", { userId });
+      const coParticipants = await messageRepository.findCoParticipantIds(userId);
+      coParticipants.forEach((p) => {
+        io.to(`user:${p}`).emit("presence:offline", { userId });
+      });
     }
     logger.info(`[SOCKET] Disconnected: ${socket.id} (user: ${userId})`);
   });
